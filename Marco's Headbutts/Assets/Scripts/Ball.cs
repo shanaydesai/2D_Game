d97044a2f6dd03f75b22c2c660448e14f6e60906
shanay desai <shanaydesai@email.arizona.edu>
2018-10-15 21:19:19 -0700

    Final Game

---
+++ Marco's Headbutts/Assets/Scripts/Ball.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+	[SerializeField] Paddle paddle;
+	[SerializeField] float xPush = 2f;
+	[SerializeField] float yPush = 15f;
+	[SerializeField] AudioClip[] ballSounds;
+	[SerializeField] float randomFactor = 0.2f;
+
+	Vector2 paddleToBallVector;
+	bool hasStarted = false;
+	// Use this for initialization
+	AudioSource myAudioSource;
+	Rigidbody2D myRigidBody2D;
+
+	void Start () {
+		paddleToBallVector = transform.position - paddle.transform.position;
+		myAudioSource = GetComponent<AudioSource>();
+		myRigidBody2D = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!hasStarted){
+		    LockBallToPaddle();
+			LaunchOnMouseClick();
+		}
+	}
+
+	private void LaunchOnMouseClick()
+	{
+		if (Input.GetMouseButtonDown(0)){
+			hasStarted = true;
+			myRigidBody2D.velocity = new Vector2(xPush,yPush);
+		}
+	}
+	private void LockBallToPaddle(){
+		Vector2 paddlePos = new Vector2(paddle.transform.position.x, paddle.transform.position.y);
+		transform.position = paddlePos + paddleToBallVector;
+	}
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		Vector2 velocityTweak = new Vector2(Random.Range(0f, randomFactor), Random.Range(0f, randomFactor));
+
+		if (hasStarted)
+		{
+			AudioClip clip = ballSounds[UnityEngine.Random.Range(0, ballSounds.Length)];
+			myAudioSource.PlayOneShot(clip);
+			myRigidBody2D.velocity += velocityTweak;
+		}
+	}
+}
---
+++ Marco's Headbutts/Assets/Scripts/Block.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour {
+	[SerializeField] AudioClip breakSound;
+	[SerializeField] GameObject blockSparklesVFX;
+	[SerializeField] int maxHits;
+	[SerializeField] Sprite[] hitSprites;
+
+	Level level;
+	
+	[SerializeField] int timesHit;
+
+	private void Start()
+	{
+		CountBreakableBlocks();
+	}
+
+	private void CountBreakableBlocks()
+	{
+		level = FindObjectOfType<Level>();
+		if (tag == "Breakable")
+		{
+			level.CountBreakableBlocks();
+		}
+
+	}
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		HandleHit();
+	}
+
+	private void HandleHit()
+	{
+		timesHit++;
+		if (timesHit >= maxHits)
+		{
+			DestroyBlock();
+		}
+		else
+		{
+			ShowNextHitSprite();
+		}
+	}
+
+	private void ShowNextHitSprite()
+	{
+		int spriteIndex = timesHit - 1;
+		GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
+	}
+
+	private void DestroyBlock()
+	{	
+		PlayBlockDestroySFX();
+		Destroy(gameObject);
+		level.BlockDestroyed();
+		TriggerSparklesVFX();
+	}
+
+	private void PlayBlockDestroySFX()
+	{
+		FindObjectOfType<GameStatus>().AddToScore();
+		AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
+	}
+
+	private void TriggerSparklesVFX()
+	{
+		GameObject sparkles = Instantiate(blockSparklesVFX, transform.position, transform.rotation);
+		Destroy(sparkles,1f);
+	}
+}
